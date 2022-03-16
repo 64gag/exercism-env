837cdfc7d9d22d6c88c9c157c86d170bf45ff884
Gael Aguiar <gael.aguiar@kymasoft.com>
2022-03-16 15:34:20 +0100

    Add help/doc message

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-exercism configure --token=${EXERCISM_AUTH_TOKEN}
+if ! exercism configure --token=${EXERCISM_AUTH_TOKEN} ; then
+    echo ""
+    echo "You can pass your token in the EXERCISM_AUTH_TOKEN environment variable when running the container"
+    echo "Example:"
+    echo ""
+    echo "docker container run --rm -it -e EXERCISM_AUTH_TOKEN=XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX 64gag/exercism-venv-base bash"
+    echo ""
+fi
+
 exec "$@"
